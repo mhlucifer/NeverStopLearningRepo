d0119277e3803d0b8c62565428c30dfa4b99faf0
tomato <2418378617@qq.com>
2025-08-03 23:07:00 +0800

    add C++ vector part 1

--- C++专项训练/专题训练/Vector普通和智能指针/vector的普通和安全实现方式/vector的普通和安全实现方式.cpp
+++ C++专项训练/专题训练/Vector普通和智能指针/vector的普通和安全实现方式/vector的普通和安全实现方式.cpp
@@ -1,32 +1,70 @@
 ﻿// vector的普通和安全实现方式.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 #include <iostream>
 
-class my_Vector
+
+class MyVector
 {
 private:
-	int* data;
-	size_t size;
-	size_t capcaity;
-
-public:
-	my_Vector(const int* data, size_t size, size_t capcaity) :data(nullptr), size(0), capcaity(0) {};
-	void push_back(int value)
+	int* m_data;
+	size_t m_size;
+	size_t m_capacity;
+	void reserve(size_t new_capacity)
 	{
-		if (size > capcaity)
+		if (new_capacity <= m_capacity)return;
+		int* new_data = new int[new_capacity];
+		for (int i = 0; i < m_size; i++)
 		{
-			int* new_data = new int[capcaity * 2];
-			for (int i = 0; i < size; i++)
-			{
-				new_data[i] = data[i];
-			}
-			
+			new_data[i] = m_data[i];
 		}
+		delete[] m_data;
+		m_data = new_data;
+		m_capacity = new_capacity;
+		new_data = nullptr;
 	}
 
+public:
+	MyVector() :m_data(nullptr), m_size(0), m_capacity(0) {};
+	~MyVector()
+	{
+		delete[] m_data;
+	}
+
+	void push_back(const int& value)
+	{
+		if (m_size == m_capacity)
+		{
+			size_t new_cap = (m_capacity == 0) ? 1 : m_capacity * 2;
+			reserve(new_cap);
+		}
+		m_data[m_size++] = value;
+	}
+
+	size_t size() const
+	{
+		return m_size;
+	}
+
+	size_t capacity() const
+	{
+		return  m_capacity;
+	}
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	MyVector vec;
+	std::cout << "初始状态 -> "
+		<< "Size: " << vec.size()
+		<< ", Capacity: " << vec.capacity() << std::endl;
+
+	std::cout << "\n开始添加元素..." << std::endl;
+	for (int i = 0; i < 5; ++i) {
+		vec.push_back(i);
+		std::cout << "push_back(" << i << ") 后 -> "
+			<< "Size: " << vec.size()
+			<< ", Capacity: " << vec.capacity() << std::endl;
+	}
+
+	return 0;
 }
 
