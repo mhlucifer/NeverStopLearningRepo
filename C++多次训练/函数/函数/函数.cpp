7506aff63ce6ad3b77ac6acf41319fbe67a5aaf1
Tang Tang <2418378617@qq.com>
2025-08-23 21:23:37 +0800

    add C++ function

---
+++ C++多次训练/函数/函数/函数.cpp
@@ -0,0 +1,17 @@
+﻿// 函数.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+int sub(int &a, int &b)
+{
+    return a - b;
+}
+int main()
+{
+    int w = 10;
+    int q = 11;
+    int a =sub(w, q);
+    std::cout << "Hello World!\n"<<std::endl;
+    std::cout << a<<std::endl;
+}
+
